f347e1a9acea3fd4859a5ecc5ee45754c02daa85
michaellikaiye <mye1@stuy.edu>
2018-10-30 23:15:38 -0400

    mic work

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # MKS65C-stat
+2018-10-30 stat (not cat) your files!
+There is a function called stat. It is used to get information about a file. With your table buddy, write a program that uses stat to display the following information about a file:
+file size
+mode (permissions)
+time of last access (displayed in the following format: Fri Oct 28 09:03:01 2016
+Note
+You may hardcode the file that your program uses, make sure the program works when I download and run it.
+If only there were some file that you know I will have and where it is... hmm...
+Once you have that, try the following:
+Modify the size so it is printed out in human readable form (that is using B, KB, MB, GB).
+You may want to look into the sprintf function to help.
+Attempt to print out the permissions in ls -l form, ignoring the early bits that include the file type information
+Permissions are of type mode_t, which is an integer type.
+The permissions section of the mode is stored in the last 9 bits of the mode_t data type.
+
+Github repo:
+MKS65C-stat
--- stat.c
+++ stat.c
@@ -1,16 +1,3 @@
-/* file size */
-/* mode (permissions) */
-/* time of last access (displayed in the following format: Fri Oct 28 09:03:01 2016 */
-/* Note */
-/* You may hardcode the file that your program uses, make sure the program works when I download and run it. */
-/* If only there were some file that you know I will have and where it is... hmm... */
-/* Once you have that, try the following: */
-/* Modify the size so it is printed out in human readable form (that is using B, KB, MB, GB). */
-/* You may want to look into the sprintf function to help. */
-/* Attempt to print out the permissions in ls -l form, ignoring the early bits that include the file type information */
-/* Permissions are of type mode_t, which is an integer type. */
-/* The permissions section of the mode is stored in the last 9 bits of the mode_t data type. */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
@@ -18,49 +5,59 @@
 #include <fcntl.h>
 #include <string.h>
 #include <time.h>
-
-void statP(char * str) {
-	struct stat * pter = malloc(sizeof(struct stat)); // allocate some memory
-	stat(str, pter); // record metadata into pter
-
-	// printing file info:
-	printf("file: temp.txt\n");
-
-	// read contents(just to practice reading)
-	int straw = open("temp.txt", O_RDONLY);
-	char arr[10];
-	read(straw, arr, 10);
-	close(straw);
-
-	printf("contents: %s", arr);
-	printf("size: %ld\n", pter -> st_size);
-	printf("mode(permissions): %o\n", pter -> st_mode);
-	char temp[100];
-	strftime(temp, 100, "%c", localtime(& (pter -> st_atime)));
-	printf("time accessed: %s\n", temp);
+void printHelper(int fileMode, int b, int c, char p) {
+  if ((fileMode & b) && (fileMode & c))
+    printf("%c",p);
+  else
+    printf("-");
 }
 
-int main() {
-	// int straw = open("teemp.txt", O_CREAT | O_RDWR, 0666);
-	// int st[3];
-	// st[0] = 1;
-	// st[1] = 3;
-	// st[2] = 5;
-	// //   int i;
-	// //   for
-	// write(straw, st, sizeof(int));
-	// close(straw);
-	//
-	// int * nums = malloc(4);
-	// straw = open("teemp.txt", O_RDONLY);
-	// read(straw, nums, 4);
-	// close(straw);
-	// printf("%d\n", * nums + 5);
-	//   straw = open("temp.txt", O_RDONLY);
-	//   statP(straw);
+void printPermissions(int fileMode) {
+  printf("-");
+  /* owner permissions */
+  printHelper(fileMode, S_IRUSR, S_IREAD, 'r');
+  printHelper(fileMode, S_IWUSR, S_IWRITE, 'w');
+  printHelper(fileMode, S_IXUSR, S_IEXEC, 'x');
+  /* group permissions */
+  printHelper(fileMode, S_IRGRP, S_IREAD, 'r');
+  printHelper(fileMode, S_IWGRP, S_IWRITE, 'w');
+  printHelper(fileMode, S_IXGRP, S_IEXEC, 'x');
+  /* other user permissions */
+  printHelper(fileMode, S_IROTH, S_IREAD, 'r');
+  printHelper(fileMode, S_IWOTH, S_IWRITE, 'w');
+  printHelper(fileMode, S_IXOTH, S_IEXEC, 'x');
+}
 
-	// printing results
-	printf("_________________________\n");
-	statP("temp.txt");
-	return 0;
+void statP(char * str) {
+  struct stat * pter = malloc(sizeof(struct stat)); // allocate some memory
+  stat(str, pter); // record metadata into pter
+  // printing file info:
+  printf("file: %s\n", str);
+  long int size = pter -> st_size;
+  if(size < 1000)
+    printf("size: %ld B\n", size);
+  else if (size < 1000000)
+    printf("size: %ld.%ld KB\n", size/1000, size%1000);
+  else if (size < 1000000000)
+    printf("size: %ld.%ld MB\n", size/1000000, size%1000000);
+  else
+    printf("size: %ld.%ld GB\n", size/1000000000, size%1000000000);
+  printf("mode(permissions): (%3o)\n", pter -> st_mode&0777);
+  printPermissions(pter -> st_mode);
+  char temp[100];
+  strftime(temp, 100, "%c", localtime(&(pter -> st_atime)));
+  printf("\ntime accessed: %s\n", temp);
+  free(pter);
+}
+
+
+
+
+int main() {
+  // printing results
+  printf("_________________________\n");
+  statP("temp.txt");
+  printf("_________________________\n");
+  statP("README.md");
+  return 0;
 }
